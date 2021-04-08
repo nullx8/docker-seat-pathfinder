28a88affa6cb9441ddc547abfe62a9eb6483d11c
Brett Costabile <kaezontpa@gmail.com>
2021-04-08 02:12:20 -0400

    fixing db user perms

--- config/sql/00_start.sql
+++ config/sql/00_start.sql
@@ -2,6 +2,6 @@ CREATE DATABASE pathfinder;
 CREATE DATABASE eve_universe;
 CREATE DATABASE seat;
 
-GRANT ALL PRIVILEGES ON pathfinder.* TO 'seat'@'localhost';
-GRANT ALL PRIVILEGES ON eve_universe.* TO 'seat'@'localhost';
-GRANT ALL PRIVILEGES ON seat.* TO 'seat'@'localhost';
+GRANT ALL PRIVILEGES ON pathfinder.* TO 'seat'@'%';
+GRANT ALL PRIVILEGES ON eve_universe.* TO 'seat'@'%';
+GRANT ALL PRIVILEGES ON seat.* TO 'seat'@'%';
