6bd2ae0320d83624c8f539c023b06b1c9545cb6d
Brett Costabile <kaezontpa@gmail.com>
2021-04-10 14:53:49 -0400

    forgot to remove default conf

--- nginx/nginx-entrypoint.sh
+++ nginx/nginx-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set e
 
+rm /etc/nginx/conf.d/default.conf
+
 for f in /etc/nginx/conf.d/templates/*.conf; do
     FILENAME=$(basename $f | cut -f 1 -d '.')
     envsubst < "${f}" '$NGINX_DOMAIN$NGINX_ADMIN_IP' > "/etc/nginx/conf.d/${FILENAME}.conf"
