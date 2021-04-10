9e16eb2e4027b8be7dc5613ceb8b4e919010ccfe
Brett Costabile <kaezontpa@gmail.com>
2021-04-10 14:33:22 -0400

    change variable name

--- nginx/nginx-entrypoint.sh
+++ nginx/nginx-entrypoint.sh
@@ -3,7 +3,7 @@ set e
 
 for f in /etc/nginx/conf.d/templates/*.conf; do
     FILENAME=$(basename $f | cut -f 1 -d '.')
-    envsubst < "${f}" '$NGINX_HOST$NGINX_ADMIN_IP' > "/etc/nginx/conf.d/${FILENAME}.conf"
+    envsubst < "${f}" '$NGINX_DOMAIN$NGINX_ADMIN_IP' > "/etc/nginx/conf.d/${FILENAME}.conf"
 done
 
 nginx -g 'daemon off;'
