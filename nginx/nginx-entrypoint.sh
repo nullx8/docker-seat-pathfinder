23074c4b1853e0642a250199ab4bd0666a83e231
Brett Costabile <kaezontpa@gmail.com>
2021-04-09 21:44:04 -0400

    fixes

--- nginx/nginx-entrypoint.sh
+++ nginx/nginx-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set e
 
-for f in ls /etc/nginx/conf.d/templates/*.conf; do
+for f in /etc/nginx/conf.d/templates/*.conf; do
     FILENAME=$(basename $f | cut -f 1 -d '.')
-    envsubst < "${f}" '$NGINX_HOST$NGINX_ADMIN_IP' > "/etc/nginx/conf.d/${FILENAME}"
+    envsubst < "${f}" '$NGINX_HOST$NGINX_ADMIN_IP' > "/etc/nginx/conf.d/${FILENAME}.conf"
 done
 
 nginx -g 'daemon off;'
